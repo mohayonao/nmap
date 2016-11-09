173b69ee43725c4150d752c5b8afc39e26fa67b8
mohayonao <mohayonao@gmail.com>
2016-11-10 06:50:27 +0900

    change callback arguments: undefined -> array index

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-module.exports = function nmap(n, map) {
+"use strict";
+
+function nmap(n, map) {
   var result = new Array(n);
 
   for (var i = 0; i < n; i++) {
-    result[i] = map(result[i], i, result);
+    result[i] = map(i, i, result);
   }
 
   return result;
-};
+}
+
+module.exports = nmap;
--- test.js
+++ test.js
@@ -4,4 +4,5 @@ const assert = require("assert");
 const nmap = require(".");
 
 assert.deepEqual(nmap(4, () => 0), [ 0, 0, 0, 0 ]);
+assert.deepEqual(nmap(4, (i) => i), [ 0, 1, 2, 3 ]);
 assert.deepEqual(nmap(4, (_, i) => i), [ 0, 1, 2, 3 ]);
